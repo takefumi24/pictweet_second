46a693d9143283f9fcfac0d87a5abac0b7ca8cc5
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-30 23:21:40 +0900

    リダイレクト

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
   before_action :set_tweet, only: [:edit, :show]
+  #未ログイン時で新規投稿画面に直接アクセスさせない。無限ループを防ぐために必要なアクション、また、詳細ページへはログインする必要がない
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @tweets = Tweet.all
@@ -36,5 +38,9 @@ class TweetsController < ApplicationController
 
   def set_tweet
     @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end
