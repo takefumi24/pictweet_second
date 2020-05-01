344ab3913ded4556a6c3b19ca0ab5b4832a90233
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-01 12:43:55 +0900

    tweetsテーブルから「nameカラム」を削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_30_150507) do
+ActiveRecord::Schema.define(version: 2020_05_01_034049) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
     t.string "text"
     t.text "image"
     t.datetime "created_at", null: false
