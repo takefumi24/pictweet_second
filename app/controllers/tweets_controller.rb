1cc6f4009a4840b8d9583b30ca1dcbd1f70580b9
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-01 12:44:10 +0900

    includeメソッド

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,7 +5,7 @@ class TweetsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @tweets = Tweet.all
+    @tweets = Tweet.includes(:user)
   end
 
   def new
@@ -34,7 +34,7 @@ class TweetsController < ApplicationController
 
   private
   def tweet_params
-    params.require(:tweet).permit(:name, :image, :text).merge(user_id: current_user.id)
+    params.require(:tweet).permit(:image, :text).merge(user_id: current_user.id)
   end
 
   def set_tweet
