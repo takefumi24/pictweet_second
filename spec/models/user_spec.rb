ad79c110e9a7103548ea78318b313b952d02c57b
takefumi24 <t_ohba24@yahoo.co.jp>
2020-05-09 13:09:38 +0900

    「nickname」と「email」が空の場合登録できないことを確かめるテストコード

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+describe User do
+  describe '#create' do
+    it "nicknameがない場合は登録ができないこと" do
+     user = User.new(nickname: "", email: "kkk@gmail.com", password: "00000000", password_confirmation: "00000000")
+     user.valid?
+     expect(user.errors[:nickname]).to include("can't be blank")
+    end
+
+    it "emailがない場合は登録できないこと" do
+      user = User.new(nickname: "dan", email: "", password: "00000000", password_confirmation: "00000000")
+      user.valid?
+      expect(user.errors[:email]).to include("can't be blank")
+    end
+  end
+end
